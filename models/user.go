1f6422b458aad1e4d3d2f398154338ca8944d9c8
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-08 15:27:27 +0700

    update user model

--- models/user.go
+++ models/user.go
@@ -2,9 +2,11 @@ package models
 
 import "github.com/jinzhu/gorm"
 
+// User struct represents the user model with an active field.
 type User struct {
 	gorm.Model
 	Username string `json:"username" binding:"required,min=3,max=30"`
 	Password string `json:"password" binding:"required,min=6"`
-	Role string `json:"role"`
+	Role     string `json:"role"`
+	Active   bool   `json:"active" gorm:"default:false"` // Active field with a default value of false
 }
