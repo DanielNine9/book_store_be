81e82905fcd0457702565f73b6671fdfd5a3b176
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-09 15:16:18 +0700

    Create transaction.go

---
+++ models/transaction.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+    "time"
+    "github.com/jinzhu/gorm"
+)
+
+type Transaction struct {
+    gorm.Model
+    UserID          uint        `json:"user_id"`
+    TotalAmount     float64     `json:"total_amount"`
+    Status          string      `json:"status"`
+    TransactionTime time.Time   `json:"transaction_time"`
+    Purchases       []Purchase  `json:"purchases"`
+    User            User        `json:"user"`
+	
+}
+func (t *Transaction) BeforeCreate(tx *gorm.DB) (err error) {
+    if t.TransactionTime.IsZero() {
+        t.TransactionTime = time.Now() 
+    }
+    return nil
+}
