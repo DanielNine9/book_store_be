bbbe408fcb1415108d8125ef3c2ba59dc153028c
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-09 15:16:16 +0700

    Update purchase.go

--- models/purchase.go
+++ models/purchase.go
@@ -4,9 +4,12 @@ import "github.com/jinzhu/gorm"
 
 type Purchase struct {
     gorm.Model
-    UserID   uint    `json:"user_id"`
-    BookID   uint    `json:"book_id"`
-    Quantity uint    `json:"quantity" gorm:"default:1"`  
-    User     User    `json:"user"`
-    Book     Book    `json:"book"`
+    UserID      uint    `json:"user_id"`
+    BookID      uint    `json:"book_id"`
+    Quantity    uint    `json:"quantity" gorm:"default:1"`  
+    User        User    `json:"user"`
+    Book        Book    `json:"book"`
+    TransactionID uint   `json:"transaction_id"` 
+    BookPrice   float64 `json:"book_price" gorm:"default:1`     
+    Transaction Transaction `json:"transaction"`    
 }
