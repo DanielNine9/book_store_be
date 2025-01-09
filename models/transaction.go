408f2379939160f7a04b250949af47e747e6fe81
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-09 16:17:31 +0700

    Update transaction.go

--- models/transaction.go
+++ models/transaction.go
@@ -1,3 +1,27 @@
+// package models
+
+// import (
+//     "time"
+//     "github.com/jinzhu/gorm"
+// )
+
+// type Transaction struct {
+//     gorm.Model
+//     UserID          uint        `json:"user_id"`
+//     TotalAmount     float64     `json:"total_amount"`
+//     Status          string      `json:"status"`
+//     TransactionTime time.Time   `json:"transaction_time"`
+//     Purchases       []Purchase  `json:"purchases"`
+//     User            User        `json:"user"`
+	
+// }
+// func (t *Transaction) BeforeCreate(tx *gorm.DB) (err error) {
+//     if t.TransactionTime.IsZero() {
+//         t.TransactionTime = time.Now() 
+//     }
+//     return nil
+// }
+
 package models
 
 import (
@@ -5,19 +29,28 @@ import (
     "github.com/jinzhu/gorm"
 )
 
+type TransactionStatus string
+
+const (
+    Pending   TransactionStatus = "pending"
+    Approved  TransactionStatus = "approved"
+    Rejected  TransactionStatus = "rejected"
+    Completed TransactionStatus = "completed"
+)
+
 type Transaction struct {
     gorm.Model
-    UserID          uint        `json:"user_id"`
-    TotalAmount     float64     `json:"total_amount"`
-    Status          string      `json:"status"`
-    TransactionTime time.Time   `json:"transaction_time"`
-    Purchases       []Purchase  `json:"purchases"`
-    User            User        `json:"user"`
-	
+    UserID          uint               `json:"user_id"`
+    TotalAmount     float64            `json:"total_amount"`
+    Status          TransactionStatus  `json:"status"`
+    TransactionTime time.Time          `json:"transaction_time"`
+    Purchases       []Purchase         `json:"purchases"`
+    User            User               `json:"user"`
 }
+
 func (t *Transaction) BeforeCreate(tx *gorm.DB) (err error) {
     if t.TransactionTime.IsZero() {
-        t.TransactionTime = time.Now() 
+        t.TransactionTime = time.Now()
     }
     return nil
 }
