6063b797c99d2a02b9084a675d538a295ab4621a
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-09 16:17:51 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"shop-account/handlers"
+	"shop-account/handlers/admin"
 	"github.com/gin-gonic/gin"
 )
 
 // SetupRoutes đăng ký tất cả các route cho API, bao gồm cả xác thực
-func SetupRoutes(router *gin.Engine, purchaseHandler *handlers.PurchaseHandler, userHandler *handlers.UserHandler, authorHandler *handlers.AuthorHandler, bookHandler *handlers.BookHandler, authHandler *handlers.AuthHandler) {
+func SetupRoutes(router *gin.Engine,adminTransactionHandler *admin.AdminTransactionHandler, transactionHandler *handlers.TransactionHandler, purchaseHandler *handlers.PurchaseHandler, userHandler *handlers.UserHandler, authorHandler *handlers.AuthorHandler, bookHandler *handlers.BookHandler, authHandler *handlers.AuthHandler) {
 	AuthorRoutes(router, authorHandler)
 
 	BookRoutes(router, bookHandler)
@@ -14,4 +15,6 @@ func SetupRoutes(router *gin.Engine, purchaseHandler *handlers.PurchaseHandler, 
 	AuthRoutes(router, authHandler)
 	UserRoutes(router, userHandler)
 	PurchaseRoutes(router, purchaseHandler)
+	TransactionRoutes(router, transactionHandler)
+	AdminRoutes(router, adminTransactionHandler)
 }
