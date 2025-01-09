6d3e6c4deac66fec05a2c513a047a4a996d5e473
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-09 16:44:01 +0700

    Update routes.go

--- routes/routes.go
+++ routes/routes.go
@@ -7,11 +7,11 @@ import (
 )
 
 // SetupRoutes đăng ký tất cả các route cho API, bao gồm cả xác thực
-func SetupRoutes(router *gin.Engine,adminTransactionHandler *admin.AdminTransactionHandler, transactionHandler *handlers.TransactionHandler, purchaseHandler *handlers.PurchaseHandler, userHandler *handlers.UserHandler, authorHandler *handlers.AuthorHandler, bookHandler *handlers.BookHandler, authHandler *handlers.AuthHandler) {
+func SetupRoutes(router *gin.Engine,categoryHandler *handlers.CategoryHandler,adminTransactionHandler *admin.AdminTransactionHandler, transactionHandler *handlers.TransactionHandler, purchaseHandler *handlers.PurchaseHandler, userHandler *handlers.UserHandler, authorHandler *handlers.AuthorHandler, bookHandler *handlers.BookHandler, authHandler *handlers.AuthHandler) {
 	AuthorRoutes(router, authorHandler)
 
 	BookRoutes(router, bookHandler)
-
+	CategoryRoutes(router, categoryHandler)
 	AuthRoutes(router, authHandler)
 	UserRoutes(router, userHandler)
 	PurchaseRoutes(router, purchaseHandler)
