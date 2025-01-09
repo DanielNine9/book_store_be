b72370302716fa34c0cce2268a631b96233f373e
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-09 16:17:35 +0700

    Create adminRoute.go

---
+++ routes/adminRoute.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"shop-account/handlers/admin"
+	"shop-account/middlewares"
+	"github.com/gin-gonic/gin"
+
+)
+
+func AdminRoutes(router *gin.Engine, adminTransactionHandler *admin.AdminTransactionHandler) {
+	adminGroup := router.Group("/admin")
+    adminGroup.Use(middlewares.AuthMiddleware())
+
+	{
+		adminGroup.GET("/transactions", adminTransactionHandler.GetAllTransactions)
+		adminGroup.PATCH("/transactions/:id/status", adminTransactionHandler.UpdateTransactionStatus)
+	}
+}
+
