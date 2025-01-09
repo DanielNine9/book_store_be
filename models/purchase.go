ab7cc19b0d72071309691f395451344b164604dd
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-09 11:12:23 +0700

    Update purchase.go

--- models/purchase.go
+++ models/purchase.go
@@ -2,11 +2,11 @@ package models
 
 import "github.com/jinzhu/gorm"
 
-// Purchase struct represents the purchase record between user and book.
 type Purchase struct {
     gorm.Model
-    UserID uint   `json:"user_id"`
-    BookID uint   `json:"book_id"`
-    User   User   `json:"user"`
-    Book   Book   `json:"book"`
+    UserID   uint    `json:"user_id"`
+    BookID   uint    `json:"book_id"`
+    Quantity uint    `json:"quantity" gorm:"default:1"`  
+    User     User    `json:"user"`
+    Book     Book    `json:"book"`
 }
