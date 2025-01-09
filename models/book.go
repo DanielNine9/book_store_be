78ceaa26fbbfdb7844ba577e7b1b9d548eb351cf
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-09 11:24:49 +0700

    Update book.go

--- models/book.go
+++ models/book.go
@@ -4,11 +4,12 @@ import "github.com/jinzhu/gorm"
 
 type Book struct {
     gorm.Model
-    Title       string  `json:"title"`
-    Description string  `json:"description"`
-    Price       float64 `json:"price" gorm:"default:0"` 
-    AuthorID    uint    `json:"author_id"`
-    Author      Author  `json:"author"`
-    
-    Active      bool    `json:"active" gorm:"default:true"`  
+    Title          string  `json:"title"`
+    Description    string  `json:"description"`
+    Price          float64 `json:"price" gorm:"default:0"` 
+    AuthorID       uint    `json:"author_id"`
+    Author         Author  `json:"author"`
+    Active         bool    `json:"active" gorm:"default:true"`
+    QuantityInStock uint   `json:"quantity_in_stock" gorm:"default:10"` 
+    QuantitySold   uint    `json:"quantity_sold" gorm:"default:0"`    
 }
