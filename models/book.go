e1918afda2cc1eb182a879ed2813bf20d43c1b3a
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-14 16:57:29 +0700

    Update book.go

--- models/book.go
+++ models/book.go
@@ -4,29 +4,35 @@ import "github.com/jinzhu/gorm"
 
 type Book struct {
     gorm.Model
-    Title           string    `json:"title"`
-    Description     string    `json:"description"`
-    Price           float64   `json:"price" gorm:"default:0"`
-    AuthorID        uint      `json:"author_id"`
-    Author          Author    `json:"author"`
-    Active          bool      `json:"active" gorm:"default:true"`
-    QuantityInStock uint      `json:"quantity_in_stock" gorm:"default:10"`
-    QuantitySold    uint      `json:"quantity_sold" gorm:"default:0"`
+    Title           string     `json:"title"`
+    Description     string     `json:"description"`
+    Price           float64    `json:"price" gorm:"default:0"`
+    AuthorID        uint       `json:"author_id"`
+    Author          Author     `json:"author"`
+    Active          bool       `json:"active" gorm:"default:true"`
+    QuantityInStock uint       `json:"quantity_in_stock" gorm:"default:10"`
+    QuantitySold    uint       `json:"quantity_sold" gorm:"default:0"`
     Categories      []Category `gorm:"many2many:book_categories;foreignkey:ID;association_foreignkey:ID" json:"categories"`
-    Code            string    `json:"code"`
+    Code            string     `json:"code"`
 
-    Publisher       string    `json:"publisher"`
-    PublicationYear int       `json:"publication_year"`
-    Weight          float64   `json:"weight"`  
-    Dimensions      string    `json:"dimensions"`  
-    Pages           int       `json:"pages"`  
-    BindingType     string    `json:"binding_type"`  
-
-    Images          []BookImage   `gorm:"foreignkey:ID" json:"images"`
+    BookDetail      BookDetail `gorm:"foreignkey:BookID" json:"book_detail"`
+    Images          []BookImage `gorm:"foreignkey:BookID" json:"images"`
 }
 
 type BookImage struct {
-    gorm.Model
     URL     string `json:"url"`
     BookID  uint   `json:"book_id"`
+}
+
+
+type BookDetail struct {
+    gorm.Model
+    Publisher       string  `json:"publisher"`
+    PublicationYear int     `json:"publication_year"`
+    Weight          float64 `json:"weight"`
+    Dimensions      string  `json:"dimensions"`
+    Pages           int     `json:"pages"`
+    BindingType     string  `json:"binding_type"`
+
+    BookID uint `json:"book_id"` 
 }
