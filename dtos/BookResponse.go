79685d0eafd55e9759ab892620e47e8db3f68a55
Đinh Huy <huy.dinh@pninformatics.com>
2025-01-14 16:57:24 +0700

    Update BookResponse.go

--- dtos/BookResponse.go
+++ dtos/BookResponse.go
@@ -12,7 +12,12 @@ type BookResponse struct {
     Description   string     `json:"description"`
     Price         float64    `json:"price"`
     Author        models.Author     `json:"author"`
+    BookDetail        models.BookDetail     `json:"book_detail"`
+    Images        []models.BookImage     `json:"images"`
     Categories    []models.Category `json:"categories"`
     IsFavorite    bool       `json:"is_favorite"` 
     IdFavorite    uint   `json:"id_favorite"`
+    Active          bool       `json:"active"`
+    QuantityInStock uint       `json:"quantity_in_stock""`
+    QuantitySold    uint       `json:"quantity_sold"`
 }
